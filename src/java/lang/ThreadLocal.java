a856fc192ed36ec195fc78feccbb5cdbca3fb172
Rocky <rocky.peng@qq.com>
2019-12-25 17:07:22 +0800

    nothing

--- src/java/lang/ThreadLocal.java
+++ src/java/lang/ThreadLocal.java
@@ -178,12 +178,7 @@ public class ThreadLocal<T> {
      */
     private T setInitialValue() {
         T value = initialValue();
-        Thread t = Thread.currentThread();
-        ThreadLocalMap map = getMap(t);
-        if (map != null)
-            map.set(this, value);
-        else
-            createMap(t, value);
+        set(value);
         return value;
     }
 
